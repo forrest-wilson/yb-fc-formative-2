aefc26fada26d62325d78846ef2d047d122ae7bc
Forrest Wilson <forrest.wilson@me.com>
2017-11-09 10:51:22 +1300

    Image change is working

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,31 @@
 // IIFE
 (function() {
-    var obj = {};
+    // Variable declarations
+    var form = document.getElementsByTagName("form"),
+        imgSelect = document.getElementById("image"),
+        bgSelect = document.getElementById("background"),
+        fontSelect = document.getElementById("font"),
+        brdrSelect = document.getElementById("border"),
+        message = document.getElementById("message"),
+        subBtn = document.getElementsByTagName("input")[0],
+        card = document.getElementsByClassName("card")[0],
+        baseDir = "assets/";
+
+    // Get the image the user wants
+    function getImage() {
+        var imgOptions = imgSelect.children,
+            imgStr = ""; // Initializes empty string every time this function is called
+
+        for (i = 0; i < imgOptions.length; i++) {
+            if (imgOptions[i].selected === true) {
+                imgStr = imgOptions[i].value;
+                card.children[0].children[0].attributes[0].textContent = baseDir + imgStr + ".jpg"
+            }
+        }
+    }
+
+    subBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        getImage();
+    });
 })();

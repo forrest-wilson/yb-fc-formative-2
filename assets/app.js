f39cfc0035f8d32974ce890113449d90d1b9590a
Forrest Wilson <forrest.wilson@me.com>
2017-11-09 13:20:50 +1300

    Made a generic code block and refactored code

--- assets/app.js
+++ assets/app.js
@@ -13,74 +13,58 @@
         subBtn = document.getElementsByTagName("input")[0],
         card = document.getElementsByClassName("card")[0];
 
-    // Get the image the user wants
-    function changeImage() {
-        var imgOptions = imgSelect.children, // Gets all the children of the imgSelect object
-            imgStr = ""; // Initializes an empty string every time this function is called
+    // Generic function for getting the value property from the select list
+    function getValue(select) {
+        var opt = select.children,
+            val = "";
 
-        for (var i = 0; i < imgOptions.length; i++) {
-            if (imgOptions[i].selected === true) {
-                imgStr = imgOptions[i].value;
-                card.children[0].children[0].attributes[0].textContent = "assets/" + imgStr + ".jpg";
+        for (var i = 0; opt.length; i++) {
+            if (opt[i].selected === true) {
+                return val = opt[i].value;
             }
         }
+    }
+
+    function changeImage() {
+        var imgVal = getValue(imgSelect);
+
+        card.children[0].children[0].attributes[0].textContent = "assets/" + imgVal + ".jpg";
     }
 
     function changeBGColor() {
-        var bgOptions = bgSelect.children, // Gets all the children of the bgSelect element
-            bgValue = ""; // Initializes an empty string every time this function is called
-        
-        for (var i = 0; i < bgOptions.length; i++) {
-            if (bgOptions[i].selected === true) {
-                bgValue = bgOptions[i].value;
-                card.classList.remove(card.classList[1]);
-                card.classList.add(bgValue + "Background");
-            }
-        }
+        var bgVal = getValue(bgSelect);
+
+        card.classList.remove(card.classList[1]);
+        card.classList.add(bgVal + "Background");
     }
 
     function changeFont() {
-        var fontOptions = fontSelect.children,
-            fontValue = "";
+        var fontVal = getValue(fontSelect);
 
-        for (var i = 0; i < fontOptions.length; i++) {
-            if (fontOptions[i].selected === true) {
-                fontValue = fontOptions[i].value;
-                card.children[1].children[0].classList = []; // Empty the classList array
-                card.children[1].children[0].classList.add(fontValue);
-            }
-        }
+        card.children[1].children[0].classList = []; // Empty the classList array
+        card.children[1].children[0].classList.add(fontVal);
     }
 
     function changeBorder() {
-        var brdrOptions = brdrSelect.children,
-            brdrValue = "";
+        var brdrVal = getValue(brdrSelect);
 
-        for (var i = 0; i < brdrOptions.length; i++) {
-            if (brdrOptions[i].selected === true) {
-                brdrValue = brdrOptions[i].value;
-
-                switch (brdrValue) {
-                    case "thin":
-                        brdrValue = "small";
-                        break;
-                    case "thick":
-                        brdrValue = "big";
-                        break;
-                    default:
-                        break;
-                }
-
-                card.children[1].classList = [];
-                card.children[1].classList.add(brdrValue + "Border");
-            }
+        switch (brdrVal) {
+            case "thin":
+                brdrVal = "small";
+                break;
+            case "thick":
+                brdrVal = "big";
+                break;
+            default:
+                break;
         }
+
+        card.children[1].classList = [];
+        card.children[1].classList.add(brdrVal + "Border");
     }
 
     function changeText() {
-        var msgValue = message.value;
-
-        card.children[1].children[0].textContent = msgValue;
+        card.children[1].children[0].textContent = message.value;
     }
 
     subBtn.addEventListener("click", function(e) {
